6865865526e33cdc3e6942333740801f47d608a9
Joseph Petrillo <jpetrillo119@gmail.com>
2025-03-28 18:02:08 -0400

    layout fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,8 +5,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-dvh flex-col">
       <Header />
-      <main className="mx-3 flex-1 border-r border-l p-3 md:mx-8 lg:mx-12">
-        {children}
+      <main className="mx-3 flex-1 border-r border-l md:mx-8 lg:mx-12">
+        <div className="mx-auto max-w-7xl px-4 py-8 md:px-6">{children}</div>
       </main>
       <Footer />
     </div>
