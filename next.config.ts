192a649ae5c3af7f471955fe92a54c256b3bb123
Joseph Petrillo <jpetrillo119@gmail.com>
2025-03-28 18:02:22 -0400

    allow rusthelp images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.rusthelp.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
