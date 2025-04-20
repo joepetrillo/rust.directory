2ee8c54901134fae4b8eb2fa60a886e876e02cf1
Joseph Petrillo <jpetrillo119@gmail.com>
2025-04-19 20:44:09 -0400

    add better auth env

--- src/env.ts
+++ src/env.ts
@@ -4,9 +4,13 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string().url(),
   },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
   },
 });
